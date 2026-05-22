92511270d1adaa7f91e6835ed33e65151af36249
Guilherme Francini <86390834+GuiFrancini@users.noreply.github.com>
2026-05-22 20:05:17 -0300

    ✨ feat: implementação da navegação

--- PokemonAPI/src/App.tsx
+++ PokemonAPI/src/App.tsx
@@ -1,8 +1,17 @@
 // src/App.tsx
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { PokemonDetailsPage } from "@/pages/pokemon-details-page";
 import { Home } from "./pages/home";
 
 function App() {
-  return <Home />;
+  return (
+    <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/pokemon/:name" element={<PokemonDetailsPage />} />
+        </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
